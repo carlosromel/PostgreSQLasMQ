5a1d46cfca563e90d59f11d0dbf744a17245541c
Carlos Romel Pereira da Silva <carlos.romel@gmail.com>
2022-11-18 13:56:52 -0300

    Melhorar a mensagem do roterio, incluindo exemplos

--- bin/pg_notify.sh
+++ bin/pg_notify.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-echo "Aguardando a existência do arquiuvo waiting.txt.$$ para iniciar os testes."
+if [ ! -f waiting.txt ]; then
+	cat <<-HERE
+	Aguardando a existência do arquivo waiting.txt para o início dos testes.
+	Abra uma quantidade suficiente de terminais com esse mesmo comando.
 
-while [ ! -f waiting.txt.$$ ]; do
+	Ex:
+	for N in 0 1 2 3 4 5 6 7 8 9; do
+	    xterm -e 'bin/pg_notify.sh'
+	done
+
+	Quando o arquivo waiting.txt passar a existir (touch waiting.txt), os
+	roteiros começarão sua execução em paralelo.
+	HERE
+fi
+
+while [ ! -f waiting.txt ]; do
     echo -n ''
 done
 
@@ -13,6 +26,7 @@ for N in $(seq 1 1000); do
          --command "notify lista, '{ "Processo": "$$", "Mensagem": "$N" }';"
 done
 
-if [ -f waiting.txt.$$ ]; then
-    rm waiting.txt.$$
+if [ -f waiting.txt ]; then
+    rm waiting.txt
 fi
+
